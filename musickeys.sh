67a0aa2c55f6b2eb0e98918e0afd00bde8137d1e
Jakub Chmura <chmura.jakub@gmail.com>
2013-04-13 15:40:14 +0200

    new scripts

---
+++ musickeys.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+case $1 in
+   "play")
+       key="XF86AudioPlay"
+       ;;
+   "next")
+       key="XF86AudioNext"
+       ;;
+   "prev")
+       key="XF86AudioPrev"
+       ;;
+   "stop")
+       key="XF86AudioStop"
+       ;;
+   *)
+       echo "Usage: $0 play|next|prev"
+       exit 1
+        ;;
+esac
+xdotool key --window $(xdotool search --name "Spotify (Premium )?- Linux Preview"|head -n1) $key
+exit 0
